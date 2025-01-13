bd7a658184acbb724b6d4d047e994321e957e3d3
Max Pestov <ealataur@gmail.com>
2025-01-13 17:28:38 +0300

    Move some node options to env

--- docker/qf-node-start.sh
+++ docker/qf-node-start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ ! -z "${QF_NODE_NAME}" ];
+then OPTIONAL_NAME=--name ${QF_NODE_NAME};
+fi
+
 /opt/qf-solochain/qf-node \
 	--port ${QF_PORT} \
 	--base-path /opt/qf-solochain/node-data/qf-devnet-node/ \
@@ -7,5 +12,6 @@
 	--rpc-methods safe \
 	--rpc-port ${QF_RPC_PORT} \
 	--rpc-cors all \
-	--telemetry-url 'wss://telemetry.qfnetwork.xyz/submit 0' \
-    --bootnodes /ip4/103.113.69.222/tcp/30333/p2p/12D3KooWMWYPNvMSSstMoADACyX7EppjuRWtTeugFcVEQcrrjvoR
+	${OPTIONAL_NAME} \
+	--telemetry-url ${QF_TELEMETRY_URL} \
+    --bootnodes /ip4/${QF_BOOT_NODE_IP}/tcp/${QF_BOOT_NODE_PORT}/p2p/${QF_BOOT_NODE_KEY}
